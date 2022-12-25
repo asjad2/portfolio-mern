aad2b696c849e0ce4fd86499b60cd1eef0f52d7c
Asjad Iftikhar <asjad2@users.noreply.github.com>
2022-12-26 03:50:16 +0500

    f

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,34 @@
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import express from 'express';
-import mongoose from 'mongoose';
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import router from "./Routes/Routes.js"
+import router from "./Routes/Routes.js";
+import dotenv from "dotenv";
+
 const app = express();
 // app.get("/",(req,res)=>{
 //   res.send("ssss")
 // })
-app.listen(5000)
-const PORT = process.env.PORT || 8080;
-const url ="mongodb+srv://asjad:123@cluster0.any5o.mongodb.net/test";
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true} )
-.then(() => console.log("Connected to the database."));
+dotenv.config({ path: "./config.env" });
 
-if (process.env.NODE_ENV==='production'){}
-app.use(express.static('frontend/build'));
-// mongoose.set('strictQuery', false);
+const url = process.env.DataBase;
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`);
+  });
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to the database."));
+
+if (process.env.NODE_ENV === "production") {
+}
+app.use(express.static("frontend/build"));
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use(bodyParser.json( { extended: true } ));
-app.use(bodyParser.urlencoded( { extended: true } ));
-app.use("/portfolio",router)
-
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/portfolio", router);
